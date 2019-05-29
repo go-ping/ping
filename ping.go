eead07883f73d5fcb860d2337013ebed94afdcd0
Alirie Gray <alirie.gray@gmail.com>
2019-05-29 13:33:44 -0700

    Fix runtime panic

--- ping.go
+++ ping.go
@@ -440,7 +440,7 @@ func (p *Pinger) processPacket(recv *packet) error {
 	var proto int
 	if p.ipv4 {
 		if p.network == "ip" {
-			bytes = ipv4Payload(recv.bytes)
+			bytes = ipv4Payload(recv)
 		} else {
 			bytes = recv.bytes
 		}
@@ -589,11 +589,13 @@ func byteSliceOfSize(n int) []byte {
 	return b
 }
 
-func ipv4Payload(b []byte) []byte {
+func ipv4Payload(recv *packet) []byte {
+	b := recv.bytes
 	if len(b) < ipv4.HeaderLen {
 		return b
 	}
 	hdrlen := int(b[0]&0x0f) << 2
+	recv.nbytes -= hdrlen
 	return b[hdrlen:]
 }
 
