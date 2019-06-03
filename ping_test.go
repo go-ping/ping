56df11e077c26de3ffb5aa397c44078a8abc6716
Greg <2653109+glinton@users.noreply.github.com>
2019-06-03 17:11:21 -0600

    Make `processPacket` more performant (#59)
    * Make processPacket more performant
    
    * Add more info for debugging to returned error
    
    * remove old benchmark
    
    * change print statement to error return

--- ping_test.go
+++ ping_test.go
@@ -1,10 +1,14 @@
 package ping
 
 import (
+	"bytes"
 	"net"
 	"runtime/debug"
 	"testing"
 	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 func TestNewPingerValid(t *testing.T) {
@@ -262,5 +266,44 @@ func AssertTrue(t *testing.T, b bool) {
 func AssertFalse(t *testing.T, b bool) {
 	if b {
 		t.Errorf("Expected False, got True, Stack:\n%s", string(debug.Stack()))
+	}
+}
+
+func BenchmarkProcessPacket(b *testing.B) {
+	pinger, _ := NewPinger("127.0.0.1")
+
+	pinger.ipv4 = true
+	pinger.addr = "127.0.0.1"
+	pinger.network = "ip4:icmp"
+	pinger.id = 123
+	pinger.Tracker = 456
+
+	t := append(timeToBytes(time.Now()), intToBytes(pinger.Tracker)...)
+	if remainSize := pinger.Size - timeSliceLength - trackerLength; remainSize > 0 {
+		t = append(t, bytes.Repeat([]byte{1}, remainSize)...)
+	}
+
+	body := &icmp.Echo{
+		ID:   pinger.id,
+		Seq:  pinger.sequence,
+		Data: t,
+	}
+
+	msg := &icmp.Message{
+		Type: ipv4.ICMPTypeEchoReply,
+		Code: 0,
+		Body: body,
+	}
+
+	msgBytes, _ := msg.Marshal(nil)
+
+	pkt := packet{
+		nbytes: len(msgBytes),
+		bytes:  msgBytes,
+		ttl:    24,
+	}
+
+	for k := 0; k < b.N; k++ {
+		pinger.processPacket(&pkt)
 	}
 }
