go-ping/ping

commit 70ede2ab323bf7364ae9a76bf4bcabcbb3a5b449

Deadlock fix (#85)
Deadlock observed when starting multiple ping routines.
It happens when writting to `recv` channel (which is full) and no active
readers for this channel.

*Example to reproduce the issue* :

	var waiter sync.WaitGroup
	pingFunc := func() {
		defer waiter.Done()

		pinger, err := ping.NewPinger("8.8.8.8")
		if err != nil {
			return
		}

		pinger.SetPrivileged(true)
		pinger.Count = 5
		pinger.Interval = time.Second
		pinger.Timeout = time.Second * 4

		pinger.Run()
	}

	for i := 0; i < 1000; i++ {
		waiter.Add(1)
		go pingFunc()
	}

	waiter.Wait() // deadlock here! (reproducible almost every time)

ping.go
@@ -471,7 +471,11 @@ func (p *Pinger) recvICMP(
 				}
 			}
 
-			recv <- &packet{bytes: bytes, nbytes: n, ttl: ttl}
+			select {
+			case <-p.done:
+				return
+			case recv <- &packet{bytes: bytes, nbytes: n, ttl: ttl}:
+			}
 		}
 	}
 }

commit a2e4b6bcf2937216cbb3c8d56da014867334b24f

Fix return on deadlock fix
Fix the return value bug introduced by #85

Signed-off-by: Ben Kochie <superq@gmail.com>

ping.go
@@ -473,7 +473,7 @@ func (p *Pinger) recvICMP(
 
 			select {
 			case <-p.done:
-				return
+				return nil
 			case recv <- &packet{bytes: bytes, nbytes: n, ttl: ttl}:
 			}
 		}